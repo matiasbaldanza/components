7fe3afda93a8ff7eaca3db020a4d87b984c6fb3e
Matias Baldanza <matiasbaldanza@gmail.com>
2023-07-17 22:12:28 -0300

    feat(menu): initial menu structure, single level

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
 import './App.css'
 
+const menuItems = [
+  {
+    name: 'Products',
+    url: '/products'
+  },
+  {
+    name: 'Solutions',
+    url: '/solutions'
+  },
+  {
+    name: 'Developers',
+    url: '/developers'
+  },
+  {
+    name: 'Resources',
+    url: '/resources'
+  },
+  {
+    name: 'Pricing',
+    url: '/pricing'
+  }
+]
+
 function App () {
   return (
-    <>
-      <div>Hola mundo</div>
-    </>
+      <ul className='flex gap-3 px-4 py-2 rounded bg-slate-200'>
+        {menuItems.map((item) => (
+          <li
+            className='px-2 py-2 rounded hover:opacity-50'
+            key={item.url}
+          >{item.name}
+          </li>
+        ))}
+      </ul>
   )
 }
 
